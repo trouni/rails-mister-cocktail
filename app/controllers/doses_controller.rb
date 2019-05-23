eea922ff70b93714d38fe48855b77620f7563987
Trouni Tiet <trouni@gmail.com>
2019-05-23 16:22:13 +0900

    created doses route and controller

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,4 @@
+class DosesController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'doses/new'
   get 'pages/index'
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
 end
