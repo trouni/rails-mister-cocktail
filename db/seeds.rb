8f1827088a68eae2a9e475e890f1da7423f47dee
Trouni Tiet <trouni@gmail.com>
2019-05-23 11:58:01 +0900

    seeds from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all if Rails.env.development?
+
+puts "Fetching ingredients from API"
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json = JSON.parse(open(url).read)
+
+puts "Creating ingredients..."
+json["drinks"].each do |drink|
+  Ingredient.create!(name: drink["strIngredient1"])
+  print "#"
+end
