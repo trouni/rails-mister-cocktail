89b6deb49dcd635685df435922e42861dc9f8eec
Trouni Tiet <trouni@gmail.com>
2019-05-23 22:33:33 +0900

    added cocktails and doses seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 require 'open-uri'
 require 'json'
+require 'faker'
 
+Cocktail.destroy_all if Rails.env.development?
 Ingredient.destroy_all if Rails.env.development?
 
+puts ""
 puts "Fetching ingredients from API"
 url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 json = JSON.parse(open(url).read)
@@ -10,5 +13,22 @@ json = JSON.parse(open(url).read)
 puts "Creating ingredients..."
 json["drinks"].each do |drink|
   Ingredient.create!(name: drink["strIngredient1"])
+  print "#"
+end
+
+puts ""
+puts "Creating cocktail names..."
+50.times do
+  Cocktail.create(name: Faker::Hipster.words(2).map { |el| el.capitalize if el[0] === el[0].downcase }.join(" "))
+  print "#"
+end
+
+puts ""
+puts "Adding ingredients to cocktails..."
+
+Cocktail.all.each do |cocktail|
+  (rand(2..7)).times do
+    Dose.create(cocktail: cocktail, ingredient: Ingredient.all.sample, description: "#{rand(1..8)} parts")
+  end
   print "#"
 end
