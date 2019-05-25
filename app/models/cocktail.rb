2c5ebe9a2a636dc4298255242730418bd48b4b32
Trouni Tiet <trouni@gmail.com>
2019-05-25 10:23:09 +0900

    implemented show for reviews on cocktail show page

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,7 @@ require 'faker'
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_many :reviews
   validates :name, presence: true, uniqueness: true
   mount_uploader :photo, PhotoUploader
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
+  validates :description, presence: true
+  validates :rating, numericality: { only_integer: true }, :inclusion => 1..5
 end
