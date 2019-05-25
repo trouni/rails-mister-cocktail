9387a8b2541250d9d54d7264857d95c3bc4ea5b5
Trouni Tiet <trouni@gmail.com>
2019-05-25 10:42:58 +0900

    added .env to gitignore and installed stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,19 @@
+// Import plugins
 import 'bootstrap';
-import { initUpdateNavbarOnScroll } from 'components/navbar';
-import { initSelect2 } from 'components/init_select2';
+import { Application } from "stimulus"
+import { autoload } from "stimulus/webpack-helpers"
 import 'select2/dist/css/select2.css';
 
+// Import components
+import { initSelect2 } from 'components/init_select2';
+import { initUpdateNavbarOnScroll } from 'components/navbar';
+
+// Load stimulus controllers
+const application = Application.start();
+const controllers = require.context("./controllers", true, /\.js$/);
+autoload(controllers, application);
+
+// Initialize scripts
 initUpdateNavbarOnScroll();
 initSelect2();
 
