bb82bb5ba49aac0dd78f3da75679118ced1d62f9
Trouni Tiet <trouni@gmail.com>
2019-05-25 17:28:48 +0900

    trying to fix precompliing assets failure from heroku push

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module RailsMisterCocktail
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # assets compile without app attempting to connect to the database
+    config.assets.initialize_on_precompile = false
   end
 end
